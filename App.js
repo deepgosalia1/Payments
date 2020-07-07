6a5c50e13e3d67e700c4a389b7a46c29ae99bdaa
Navkar Shah <shahnavkar15@gmail.com>
2020-07-07 18:42:07 +0530

    drawer

--- App.js
+++ App.js
@@ -4,30 +4,19 @@ import {
   StyleSheet,
   ScrollView,
   View,
-  Text,
   StatusBar,
 } from 'react-native';
 import SCard from './src/customcore/SCard';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import DrawerScreen from './src/screens/DrawerScreens';
+import {Avatar, Text, Image} from 'react-native-paper';
 
 export default function App() {
   console.disableYellowBox = true;
   return (
-    <View style={{ flex: 1, backgroundColor: 'grey' }}>
-
-
-      {/* use the examples */}
-
-
-      <SCard type='flight' title={'Flights'} />
-      <SCard type='bus' title={'Bus'} />
-
-      {/* you can also pass in your additional styles. will get appended */}
-      <SCard type='train' title={'sample big text'} cardStyle={{ elevation: 0, width: 150 }} />
-      <SCard type='train' title={'sample big text'} textStyle={{ elevation: 0, width: 150 }} />
-
-      {/* to use the card to fetch on online image use 'toFetch' prop */}
-      <SCard toFetch={true} title={'Fetched Online'} source={'https://n3.sdlcdn.com/imgs/a/8/p/Fetch-SDL441219400-1-5190a.jpg'} />
-
-    </View>
+    <NavigationContainer>
+       <DrawerScreen />
+    </NavigationContainer>
   )
 }
---
+++ src/screens/Bus/index.js
@@ -0,0 +1,34 @@
+// React Imports
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import  LinearGradient  from 'react-native-linear-gradient';
+
+import SCard from '../../customcore/SCard';
+
+export default function Bus() {
+  console.disableYellowBox = true;
+  return (
+    <LinearGradient colors= {['#ebffff','#f0ffff','#f5ffff','#faffff']} style={{ flex: 1 }} >
+      <SCard type='flight' title={'Flights'} />
+      <SCard type='bus' title={'Bus'} />
+
+      {/* you can also pass in your additional styles. will get appended */}
+      <SCard type='train' title={'sample big text'} cardStyle={{ elevation: 0, width: 150 }} />
+      <SCard type='train' title={'sample big text'} textStyle={{ elevation: 0, width: 150 }} />
+
+      {/* to use the card to fetch on online image use 'toFetch' prop */}
+      <SCard toFetch={true} title={'Fetched Online'} source={'https://n3.sdlcdn.com/imgs/a/8/p/Fetch-SDL441219400-1-5190a.jpg'} />
+    </LinearGradient>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
