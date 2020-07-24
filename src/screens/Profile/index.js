34c5d21107d1cdd2499cc9a218253819f4b44b64
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2020-07-24 21:15:45 +0530

    Utility bills section & Profile section implemnted

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -7,25 +7,56 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
 } from 'react-native';
-import  LinearGradient  from 'react-native-linear-gradient';
-
+import LinearGradient from 'react-native-linear-gradient';
+import auth from '@react-native-firebase/auth';
 import SCard from '../../customcore/SCard';
+import { Button } from 'react-native-paper';
+import { Header } from 'react-native-elements';
 
 export default function Profile() {
-  console.disableYellowBox = true;
+  var user = auth().currentUser;
   return (
-    <LinearGradient colors= {['#ebffff','#f0ffff','#f5ffff','#faffff']} style={{ flex: 1 }} >
-      <SCard type='flight' title={'Flights'} />
-      <SCard type='bus' title={'Bus'} />
-
-      {/* you can also pass in your additional styles. will get appended */}
-      <SCard type='train' title={'sample big text'} cardStyle={{ elevation: 0, width: 150 }} />
-      <SCard type='train' title={'sample big text'} textStyle={{ elevation: 0, width: 150 }} />
-
-      {/* to use the card to fetch on online image use 'toFetch' prop */}
-      <SCard toFetch={true} title={'Fetched Online'} source={'https://n3.sdlcdn.com/imgs/a/8/p/Fetch-SDL441219400-1-5190a.jpg'} />
-    </LinearGradient>
+    <View style={{ flex: 1 }}>
+      <LinearGradient colors={['#ebffff', '#f0ffff', '#f5ffff', '#faffff']} style={{ flex: 1, padding: 10 }} >
+        <Header
+          containerStyle={{height:65, borderRadius:5, backgroundColor:'transparent'}}
+          centerComponent={<Text style={{fontSize:20, textAlignVertical:'top'}}> Profile information </Text>}
+        />
+        <View style={{padding:20, justifyContent:'center'}}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ textAlignVertical: 'center', width: '50%' }}>Name : </Text>
+            <TextInput scrollEnabled={true} style={{width:'50%', borderBottomWidth:0.5}} placeholder={'Name'} defaultValue={user.displayName} />
+          </View>
+          <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ textAlignVertical: 'center', width: '50%' }}>Email : </Text>
+            <TextInput scrollEnabled={true} style={{width:'50%', borderBottomWidth:0.5}} placeholder={'Email'} defaultValue={user.email}/>
+          </View>
+          <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ textAlignVertical: 'center', width: '50%' }}>Contact No. : </Text>
+            <TextInput scrollEnabled={true} style={{width:'50%', borderBottomWidth:0.5}} placeholder={'Cell Number'} defaultValue={user.phoneNumber?user.phoneNumber:null}/>
+          </View>
+          <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ textAlignVertical: 'center', width: '50%' }}>Address Line 1 : </Text>
+            <TextInput scrollEnabled={true} style={{width:'50%', borderBottomWidth:0.5}} placeholder={'Apartment No. / name'} />
+          </View>
+          <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ textAlignVertical: 'center', width: '50%' }}>Address Line 2 : </Text>
+            <TextInput scrollEnabled={true} style={{width:'50%', borderBottomWidth:0.5}} placeholder={'Road Name/Locality'} />
+          </View>
+          <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ textAlignVertical: 'center', width: '50%' }}>Address Line 3: </Text>
+            <TextInput scrollEnabled={true} style={{width:'50%', borderBottomWidth:0.5}} placeholder={'Optional additional Info'} />
+          </View>
+          <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ textAlignVertical: 'center', width: '50%' }}>City: </Text>
+            <TextInput scrollEnabled={true} style={{width:'50%', borderBottomWidth:0.5}} placeholder={'City name'} />
+          </View>
+        </View>
+        <Button style={{marginVertical:20}} mode={'contained'}> Save and Update </Button>
+      </LinearGradient>
+    </View>
   )
 }
 
